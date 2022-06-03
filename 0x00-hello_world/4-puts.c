45ff8d76c732a66c1ad3f0a3538fde52d0962a73
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-06-03 01:22:47 -0700

    my fifth task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
-
 /**
- *  *  * main - Entry point
- *   *   *
- *    *   *Return: Always 0 (Success)
- *     **/
+ * main - Entry point
+ *
+ * puts does the printing job
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	       puts("\"Programming is like building a multilingual puzzle");
-	               return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

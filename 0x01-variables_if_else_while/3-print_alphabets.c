28ce2bb0c73e0dd44e13eb8c148bf8d439dc0d58
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-06-06 06:09:53 -0700

    task 3 of c programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print the alphabet in lower and upper
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ++ch)
+		putchar(ch);
+	return (0);
+}

e576a9e767916aa5273ed2869936b481e3ac6640
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-06-06 04:41:39 -0700

    task 2 of c programming variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * print the alpabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void){
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		putchar(ch);
+
+	return 0;
+}

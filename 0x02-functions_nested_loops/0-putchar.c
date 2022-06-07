5955d0c460443603f1a5ac194891236fb0b8c055
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-06-07 11:45:19 -0700

    task o

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[9] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(i[c]);
+	}
+	_putchar('\n');
+
+	return (0);
+}

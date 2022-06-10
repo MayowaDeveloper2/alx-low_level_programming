e501e6119a428457ad54f4c939a3d34220e95b4c
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-06-10 07:58:34 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * _putchar - print function
+ *
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch < 58; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}

c8272a3ea7a84c5138ea46972a91422ad5c05d2a
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-06-13 12:17:10 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - function that return the length of string
+ *
+ * @s: char type pointer
+ *
+ * Return: string length 
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+	return (i);
+}

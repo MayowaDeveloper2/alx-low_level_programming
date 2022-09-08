9f39a07a9cf8643e59f53dfc1db9f279ce6e555e
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-09-08 15:51:22 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,7 @@
 
 /**
  * main - This program prints the size of various types
- *
- * on the computer it is compiled and run on
- *
+ * on the computer it is compiled and run on.
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -15,5 +13,5 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-	retur (0);
+	return (0);
 }

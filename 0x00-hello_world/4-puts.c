72011b124ab7a566bbc697b845113337c7426c0d
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-09-08 15:20:24 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This prints excatly the programming
+ *
+ * is like building a multilingual puzzle
+ *
+ * followed by a new line
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}

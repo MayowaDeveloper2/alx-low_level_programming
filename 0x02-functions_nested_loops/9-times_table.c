e5fa8690b04ed62b86a708ac080be495052b5d96
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-09-14 07:57:41 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * starting with 0
+ */
+void times_table(void)
+{
+
+	int x, y, a, b;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			a = (x * y) / 10;
+			b = (x * y) % 10;
+
+			if ((x * y) > 9)
+				_putchar(a + '0');
+
+			_putchar(b + '0');
+
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+
+			if (y == 9 && x == 0)
+				break;
+			else if (x * (y + 1) < 10)
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}

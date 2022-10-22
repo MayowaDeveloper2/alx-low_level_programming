b45d12adf771c6dd8430c2872ba157bce800a6af
MayowaDeveloper2 <akintoyemayowa1@gmail.com>
2022-10-21 17:45:31 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,9 +7,10 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc string)
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
  * Description: singly linked list node structure
  */
 typedef struct list_s
